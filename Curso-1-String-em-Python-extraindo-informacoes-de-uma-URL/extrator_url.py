d25f6f7389fb110283ae7e85ccb9581bfd42c090
Ike Gabriel <ikekenard@gmail.com>
2024-07-23 10:39:44 -0300

    Utilizando metodos de strings

--- Curso-1-String-em-Python-extraindo-informacoes-de-uma-URL/extrator_url.py
+++ Curso-1-String-em-Python-extraindo-informacoes-de-uma-URL/extrator_url.py
@@ -1,9 +1,24 @@
-url = "bytebank.com/cambio?moedaOrigem=real"
-
+# Url abse
+url = "bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar"
 print(f'\nUrl: {url}\n')
 
-url_base = url[0:19]
-print(f'Url base: {url_base}')
-
-url_parametros = url[20:]
+# Separa base e parametros 
+interrogacao = url.find('?')
+url_base = url[:interrogacao]
+url_parametros = url[interrogacao + 1:]
 print(f'Parametros da url: {url_parametros}')
+
+# Busca o valor de um parametro por meio da separção '?' '&'
+parametro_busca = 'quantidade'
+indice_parametro = url_parametros.find(parametro_busca)
+indice_valor = indice_parametro + len(parametro_busca) + 1
+indice_e_comercial = url_parametros.find('&',indice_valor)
+
+# Condicional para ele separar ou não com o '&'
+if indice_e_comercial == -1:
+     valor = url_parametros[indice_valor:]
+else:
+     valor = url_parametros[indice_valor:indice_e_comercial]
+
+print(f'Valor da moeda Origem: {(valor).title()}')
+
